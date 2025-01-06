81931a29f61fa858767305afdaaa91019b9c8b15
jpestovich <jpestovich@protonmail.com>
2025-01-06 12:58:47 -0700

    adds functions for counting chars and letters

--- main.py
+++ main.py
@@ -3,6 +3,26 @@ def count_words(book):
     word_count = len(words)
     return word_count
 
+def count_characters(file_contents):
+    characters = {
+            }
+    for character in file_contents:
+        character = character.lower()
+        characters[character] = characters.get(character, 0) + 1
+    print("------------------------")
+    print("Character counts below")
+    print(characters)
+
+def count_letters(file_contents):
+    letters = {}
+    for character in file_contents:
+        if(character.isalpha()):
+            letter = character.lower()
+            letters[letter] = letters.get(letter, 0) + 1
+    print("------------------------")
+    print("Letter counts below")
+    print(letters)
+
 def main():
     book = 'books/frankenstein.txt'
     with open(book) as f:
@@ -11,5 +31,8 @@ def main():
     wc = count_words(file_contents)
     print("-----------------------------------")
     print(f"total number of words in book {wc}")
+    print("-----------------------------------")
+    count_characters(file_contents)
+    count_letters(file_contents)
 
 main()
